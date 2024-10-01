f84d746ff1ea71e24a9d4c19a304aea553eca5f9
Alfred Ramirez <alfrednicramirez@gmail.com>
2024-09-30 22:30:06 -0400

    Main Menu fix

--- Assets/Scripts/Alfred_Scripts/MainMenuManager.cs
+++ Assets/Scripts/Alfred_Scripts/MainMenuManager.cs
@@ -7,7 +7,7 @@ public class MainMenuManager : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("AlfredPrototyping");
+        SceneManager.LoadScene("SheikhPrototyping");
     }
 
     public void BackToMain()
