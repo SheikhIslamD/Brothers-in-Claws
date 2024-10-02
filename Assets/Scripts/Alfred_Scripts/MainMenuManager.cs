83cfd26e8ff04c89af5b5e8b9cf18fee28cb1897
Sheikh Islam <143849374+SheikhIslamD@users.noreply.github.com>
2024-10-02 03:57:50 -0400

    Small fix
    had wrong buildindex number in script, and had to assign to the button

--- Assets/Scripts/Alfred_Scripts/MainMenuManager.cs
+++ Assets/Scripts/Alfred_Scripts/MainMenuManager.cs
@@ -17,7 +17,7 @@ public class MainMenuManager : MonoBehaviour
 
     public void PlayDemo()
     {
-        SceneManager.LoadScene(4);
+        SceneManager.LoadScene(3);
     }
 
     public void QuitGame()
